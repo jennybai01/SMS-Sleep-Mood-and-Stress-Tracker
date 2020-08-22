fae926ad8e8f99030ff4c7e1add9f80afdf76b05
jennybai01 <55304664+jennybai01@users.noreply.github.com>
2020-08-22 19:27:42 -0400

    Update db.py

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import sqlite3
 from sqlite3 import Error
+import datetime
 
 def create_connection(path):
     connection = None
@@ -32,8 +33,16 @@ CREATE TABLE IF NOT EXISTS clients_info (
     sleep INTEGER
     );
 """
+while True: # get number
+    try:
+        number = input("Enter your phone number: ")
+        number = int(number)
+        break
+    except ValueError:
+        print("Please enter an integer.")
 
-while True:
+
+while True: # get mood
     try:
         mood = input("Rate your mood today from 1 to 10 (inclusive): ")
         mood = int(mood)
@@ -42,7 +51,7 @@ while True:
     except ValueError:
         print("Please enter an integer.")
 
-while True:
+while True: # get stress
     try:
         stress = input("Rate your stress today from 1 to 10 (inclusive): ")
         stress = int(stress)
@@ -51,22 +60,21 @@ while True:
     except ValueError:
         print("Please enter an integer.")
 
-while True:
+while True: # get sleep
     try:
-        sleep = input("How many hours of sleep did you get last night?")
+        sleep = input("How many hours of sleep did you get last night? ")
         sleep = int(sleep)
         if (sleep > -1 and sleep < 25):
             break
     except ValueError:
         print("Please enter an integer.")
 
-
 # inserting records
 create_users = """
 INSERT INTO clients_info (phone_number, entry_date, mood, stress, sleep)
 VALUES
-    (123456789, 2020-08-22, {}, {}, {});
-""".format(mood, stress, sleep)
+    ({}, datetime('now'), {}, {}, {});
+""".format(number, mood, stress, sleep)
 
 execute_query(connection, create_users_table)
 execute_query(connection, create_users)
@@ -82,13 +90,27 @@ def execute_read_query(connection, query):
     except Error as e:
         print(f"The error '{e}' occurred")
 
-select_all_info = "SELECT * FROM clients_info"
-info = execute_read_query(connection, select_all_info)
+""" ignore this for now
+select_number = input("Enter the client's number who you wish to observe: ")
+# select_info will get the top 7 most recent entries (sorted in ascending order by date) for a specified phone number
+select_info = 
+SELECT * FROM
+(
+    SELECT * FROM clients_info 
+    WHERE phone_number = {} 
+    ORDER BY entry_date DESC
+    LIMIT 7
+)
+ORDER BY entry_date
+.format(select_number)
+"""
 
+select_info = "SELECT * FROM clients_info"
+info = execute_read_query(connection, select_info)
 for client in info:
     print(client)
 
 # deleting table records
 # delete_records = "DELETE FROM clients_info;"
 # execute_query(connection, delete_records)
-# print(execute_read_query(connection, select_all_info))
+# print(execute_read_query(connection, select_info))
