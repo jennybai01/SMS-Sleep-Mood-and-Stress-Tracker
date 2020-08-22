1f87cdcb5495aba2d8e44d97973b5e2cf445bd71
jennybai01 <55304664+jennybai01@users.noreply.github.com>
2020-08-22 14:45:58 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 print('Hello, World!')
+
+x = [x for x in range(100)]
+y = [y*y for y in range(100)]
+
+ax = sns.lineplot(x=x, y=y)
+plt.show()
